d467a9922a99f8cee25f48aac19c8250338f6513
ii-lab <iilab@asu.edu>
2021-09-21 17:02:50 -0700

    test lora transmit works correctly

--- lora_send_data/lora_send_data.ino
+++ lora_send_data/lora_send_data.ino
@@ -6,14 +6,34 @@
 #define RST 4
 #define DIO0 7
 #define SPREAD_FACTOR 10
-#define CENTER_FREQ 437E6
+#define CENTER_FREQ 437.5E6
 #define BANDWIDTH 62.5E3
 #define TX_POWER 4
+
+//MAX31855 config vars
+#define SS_1 12
+#define SS_2 11
 
 //function predefinitions
 void lora_init();
 void lora_transmit();
 
+void setup() {
+	Serial.begin(9600);
+	//while (!Serial);
+
+	Serial.println("t-vac lora logger");
+	lora_init();
+
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+	lora_transmit(); 
+	delay(500);
+}
+
+// lora_section inits using the given config pins does a test begin to make sure the lora radio is works properly
 void lora_init()
 {
 	LoRa.setPins(SS, RST, DIO0);
@@ -22,7 +42,8 @@ void lora_init()
 		Serial.println("Starting LoRa failed!");
 		while (1);
 	}
-}
+} 
+//for now just transmitting the string literal "test"
 void lora_transmit()
 {
 	LoRa.begin(CENTER_FREQ);
@@ -34,19 +55,4 @@ void lora_transmit()
 	LoRa.beginPacket();
 	LoRa.print("test");
 	LoRa.endPacket();
-}
-
-void setup() {
-  Serial.begin(9600);
-  //while (!Serial);
-
-  Serial.println("t-vac lora logger");
-
-  pinMode(LED_BUILTIN, OUTPUT);
-}
-
-
-
-void loop() {
- 
 }
